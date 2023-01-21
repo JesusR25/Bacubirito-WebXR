b664015d6b376aca00ec9774c336282e89b7c2e2
JesusR25 <18170429@itculiacan.edu.mx>
2023-01-21 11:38:39 -0700

    Probar en celular

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+  import { getDatabase } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-database.js";
+  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +17,5 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
+  const auth = getAuth();
