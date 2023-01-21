ef0bd72c02ecc172276fe59c8abc89bc585ddb03
JesusR25 <18170429@itculiacan.edu.mx>
2023-01-20 17:58:30 -0700

    Borrado de archivos que no se utilizan

---
+++ js/index.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAoCQK1rZdXPeQhcc9RBxcdpeyHTZbIUno",
+    authDomain: "ccar-2a74c.firebaseapp.com",
+    projectId: "ccar-2a74c",
+    storageBucket: "ccar-2a74c.appspot.com",
+    messagingSenderId: "532265497230",
+    appId: "1:532265497230:web:9cd7e947b3c72a84498fdf"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
