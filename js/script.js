6bbcee6a510722892f1f11a0ba4cc9f22397877c
JesusR25 <18170429@itculiacan.edu.mx>
2023-01-23 11:37:47 -0700

    Con marker nuevo mundrabilla

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const secondsSphere = document.getElementById('seconds-sphere');
 let stopwatchInterval;
 let runningTime = 0;
 
+
 const playPause = () => {
     const isPaused = !playPauseButton.classList.contains('running');
     if (isPaused) {
