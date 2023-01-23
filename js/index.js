8f4b06d8b29fa0f2b98524e03e117015daf77b6d
JesusR25 <18170429@itculiacan.edu.mx>
2023-01-23 10:24:59 -0700

    Registro y Login listo

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
-  import { getDatabase } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-database.js";
-  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
+  import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-database.js";
+  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +19,99 @@
   const app = initializeApp(firebaseConfig);
   const database = getDatabase(app);
   const auth = getAuth();
+
+  var botonreg = document.getElementById("registrar");
+  var botonini = document.getElementById("iniciar");
+
+  botonreg.addEventListener("click", (e) => {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('pass').value;
+    var username = document.getElementById('user').value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+
+        set(ref(database, 'users/' + user.uid),{
+          username: username,
+          email: email,
+        })
+        alert('user created!!');
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        console.log(errorMessage);
+        console.log(errorMessage);
+        alert(errorMessage);
+        // ..
+      });
+  });
+
+botonini.addEventListener("click", (e) => {
+  var email = document.getElementById('emailis').value;
+  var password = document.getElementById('passis').value;
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      const dt = new Date();
+      update(ref(database, 'users/' + user.uid),{
+        last_login: dt,
+      })
+      // ...
+      redirigir();
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      Swal.fire({
+        icon: 'error',
+        title: 'Incorrecto',
+        text: 'La contraseña y correo no coinciden',
+      });
+    });
+});
+
+const user = auth.currentUser;
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+
+function redirigir(){
+  let timerInterval;
+  Swal.fire({
+    icon: 'success',
+    title: "Inicio de sesion correcto",
+    html: "Seras redireccionado al Menu",
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const b = Swal.getHtmlContainer().querySelector("b");
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft();
+      }, 100);
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
+    },
+  }).then((result) => {
+    /* Read more about handling dismissals below */
+    if (result.dismiss === Swal.DismissReason.timer) {
+      window.location.href = "index.html";
+    }
+  });
+}
+
