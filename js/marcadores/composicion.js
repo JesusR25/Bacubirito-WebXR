fd7b7d62c1138dfbcd5ce709a0fb2b91ef18365f
JesusR25 <18170429@itculiacan.edu.mx>
2023-01-31 11:16:21 -0700

    Cambio a pagina html

--- js/marcadores/composicion.js
+++ js/marcadores/composicion.js
@@ -1,19 +1,9 @@
 var activo = false;
 var volteado = false;
-const modelList = ["cobalto", "niquel", "Hierro", "azufre"];
-const meteoros = ["metcob", "metniq", "methie", "metazu"];
-let introduccion = "El meteorito de bacubirito esta compuesto por 4 elementos. Haga click en la esfera para ver cada uno de ellos.";
-let textazu = "El meteorito de Bacubirito contiene un 0.005% de Azufre.";
-let textcob = "El meteorito de Bacubirito tambien contiene un 0.21% de Cobalto.";
-let texthie = "El meteorito de Bacubirito contiene un 88.94% de Hierro.";
-let textniq = "El meteorito de Bacubirito contiene un 6.98% de Niquel.";
-const listtext = [textcob, textniq, texthie, textazu];
-var idx = -1;
 
 function onQRCodeScanned(scannedText) {
   if (scannedText == 'Composición Química') {
     mostrar();
-    asignarflechas();
   }
   if (scannedText == "Reproducir Video") {
     document.getElementById("caja").style.display = "none";
@@ -65,57 +55,6 @@ function JsQRScannerReady() {
     //append the jbScanner to an existing DOM element
     jbScanner.appendTo(scannerParentElement);
   }
-}
-
-function asignarflechas() {
-  //Primer boton flecha
-  document.getElementById("btnAnt").addEventListener("click", (e) => {
-    if (idx <= 0) {
-      idx = 3;
-      alert(idx);
-      document.querySelector("#cob").removeAttribute("gltf-model");
-      document.querySelector("#cob").removeAttribute("scale");
-      document.querySelector("#cob").setAttribute("scale", "0.6 0.6 0.6");
-      document.querySelector("#cob").setAttribute("gltf-model", `#${modelList[idx]}`);
-      //Texto sobre informacion del material
-      document.querySelector("#parrafo").innerHTML = `#${listtext[idx]}`;
-      document.querySelector("#mod").removeAttribute("gltf-model");
-      document.querySelector("#mod").removeAttribute("scale");
-      document.querySelector("#mod").setAttribute("scale", "0.4 0.4 0.4");
-      document
-        .querySelector("#mod")
-        .setAttribute("gltf-model", `#${meteoros[idx]}`);
-    } else {
-      idx = idx - 1;
-      document.querySelector("#cob").removeAttribute("gltf-model");
-      document.querySelector("#cob").removeAttribute("scale");
-      document.querySelector("#cob").setAttribute("scale", "0.6 0.6 0.6");
-      document
-        .querySelector("#cob")
-        .setAttribute("gltf-model", `#${modelList[idx]}`);
-      //Texto sobre informacion del material
-      document.querySelector("#parrafo").innerHTML = `#${listtext[idx]}`;
-      document.querySelector("#mod").removeAttribute("gltf-model");
-      document.querySelector("#mod").removeAttribute("scale");
-      document.querySelector("#mod").setAttribute("scale", "0.4 0.4 0.4");
-      document.querySelector("#mod").setAttribute("gltf-model", `#${meteoros[idx]}`);
-    }
-  });
-  //Segundo boton flecha
-  document.getElementById("btnSig").addEventListener("click", (e) => {
-    idx = (idx + 1) % modelList.length;
-    alert(idx);
-    document.querySelector("#cob").removeAttribute("gltf-model");
-    document.querySelector("#cob").removeAttribute("scale");
-    document.querySelector("#cob").setAttribute("scale", "0.6 0.6 0.6");
-    document.querySelector("#cob").setAttribute("gltf-model", `#${modelList[idx]}`);
-    //Texto sobre informacion del material
-    document.querySelector("#parrafo").innerHTML = `#${listtext[idx]}`;
-    document.querySelector("#mod").removeAttribute("gltf-model");
-    document.querySelector("#mod").removeAttribute("scale");
-    document.querySelector("#mod").setAttribute("scale", "0.4 0.4 0.4");
-    document.querySelector("#mod").setAttribute("gltf-model", `#${meteoros[idx]}`);
-  });
 }
 
 function mostrar() {
