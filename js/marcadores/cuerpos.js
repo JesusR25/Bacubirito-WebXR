f65e6a5bcbc19083bf57a8c585e5d953fd9e675b
JesusR25 <18170429@itculiacan.edu.mx>
2023-02-02 20:46:38 -0700

    Boton volver y cinturon

--- js/marcadores/cuerpos.js
+++ js/marcadores/cuerpos.js
@@ -2,14 +2,45 @@ function onQRCodeScanned(scannedText)
 {
     if(scannedText == 'Cometa'){
         document.querySelector("#cometa").setAttribute("visible", true);
+        document.querySelector("#asteroide").setAttribute("visible", false);
         Swal.fire({
             title: 'Cometa',
             text: 'Un cometa pasara a traves de tu pantalla.',
             imageUrl: 'https://dam.ngenespanol.com/wp-content/uploads/2021/09/cometa-bernardinelli-bernstein.jpg',
-            imageWidth: 400,
-            imageHeight: 200,
+            imageWidth: 200,
+            imageHeight: 100,
             imageAlt: 'Custom image',
           })
+    }
+    if(scannedText == 'Asteroide'){
+        document.querySelector("#asteroide").setAttribute("visible", true);
+        document.querySelector("#cometa").setAttribute("visible", false);
+        Swal.fire({
+            title: 'Asteroide',
+            text: 'Un asteroide pasara a traves de tu pantalla.',
+            imageUrl: 'https://ichef.bbci.co.uk/news/640/cpsprodpb/164A7/production/_101630319_f0055887-asteroid_artwork-spl.jpg',
+            imageWidth: 200,
+            imageHeight: 100,
+            imageAlt: 'Custom image',
+          })
+    }
+    if(scannedText == 'Cinturón De Asteroides'){
+        document.querySelector("#asteroide").setAttribute("visible", false);
+        document.querySelector("#cometa").setAttribute("visible", false);
+        Swal.fire({
+            title: 'Cinturón de asteroides',
+            text: 'Para ver el cinturon de asteroides es necesario pasar a otra pagina.',
+            showCancelButton: true,
+            confirmButtonText: 'Aceptar',
+            cancelButtonText: `Cancelar`,
+          }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                window.location.href = "/pagina/cinturon.html"
+            } else if (result.isDenied) {
+              
+            }
+          });
     }
 }
 
