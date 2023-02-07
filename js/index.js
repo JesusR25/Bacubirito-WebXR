c2f04fcbfaf208d90bf44ef1603b2cca54513fdb
JesusR25 <18170429@itculiacan.edu.mx>
2023-02-07 06:48:40 -0700

    Con alert

--- js/index.js
+++ js/index.js
@@ -162,6 +162,27 @@ function invitado() {
         set(nodo, {
           Escolaridad: valor
         })
+        Swal.fire({
+          icon: 'success',
+          title: "Cargando el menu.",
+          timer: 2000,
+          timerProgressBar: true,
+          didOpen: () => {
+            Swal.showLoading();
+            const b = Swal.getHtmlContainer().querySelector("b");
+            timerInterval = setInterval(() => {
+              b.textContent = Swal.getTimerLeft();
+            }, 100);
+          },
+          willClose: () => {
+            clearInterval(timerInterval);
+          },
+        }).then((result) => {
+          /* Read more about handling dismissals below */
+          if (result.dismiss === Swal.DismissReason.timer) {
+            window.location.href = "Menu.html";
+          }
+        });
       }
     });
 }
