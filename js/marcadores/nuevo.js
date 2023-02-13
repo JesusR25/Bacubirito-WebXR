8eec43b6b9f5e5c1753df74c91da7fde8e97ce6d
Jesus Ramon Ramirez Medina <91562346+JesusR25@users.noreply.github.com>
2023-02-13 11:24:55 -0700

    Con aviso

--- js/marcadores/nuevo.js
+++ js/marcadores/nuevo.js
@@ -3,7 +3,20 @@ import { loginWithEmail, login, obtener } from "../firebase.js";
 var botonreg = document.getElementById("registrar");
 var botonini = document.getElementById("iniciar");
 var botoninv = document.getElementById("invitado");
+var botonacerca = document.getElementById("aviso");
 
+
+botonacerca.addEventListener("click", (e) => {
+  Swal
+    .fire({
+      title: "Acerca de",
+      html:'Este proyecto ha sido desarrollado para complementar la visita guiada del Meteorito de Bacubirito, actualmente ubicado en el Santuario, dentro del Centro de Ciencias de Sinaloa. <br><br> <b>Contacto</b> <br><br> aup840@gmail.com',
+      confirmButtonText: "Entendido",
+    })
+    .then(resultado => {
+  
+    });
+});
 
 botonreg.addEventListener("click", (e) => {
   var email = document.getElementById("email").value;
